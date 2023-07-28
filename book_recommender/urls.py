d3f6073700094324ec6f7536de420b8d72ea56cb
mabhishek7081 <mabhishek7081@gmail.com>
2023-07-29 01:02:48 +0530

    cleanup and readme

--- book_recommender/urls.py
+++ book_recommender/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from book_recommender.views import top_books_by_category, signup, login
 
 urlpatterns = [
-    path('', include('example.urls')),
     path('admin/', admin.site.urls),
     path('api/books_by_category/<str:category>', top_books_by_category, name='top_books_by_category'),
     path('api/signup', signup),
